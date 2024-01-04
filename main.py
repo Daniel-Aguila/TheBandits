d61f63d57e0083749f2aab8cc4b2a99e3d2ce499
Daniel Aguila <danielbaguila@gmail.com>
2024-01-03 19:03:04 -0600

    Simple method to calculate Q given action A and time T.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from argparse import Action
 import numpy as np
 import cv2
 import random
@@ -11,9 +12,22 @@ def argmax(Qa):
             max_Qa_list.append(i)
     return random.choice(max_Qa_list) #randomly choose if multiple indixes have same value
 
-def epsilon_greedy(Qa,epsilon):
+def epsilon_greedy(Qa,epsilon): #Incorporate epsilon to explore
     random_number = np.random.random()
     if random_number < epsilon:
         return random.choice(range(len(Qa)))
     else:
         return argmax(Qa)
+
+#implement QtA which gives the Q value at a current action given a certain time
+def getQtA(action, time, rewards_list, action_record):
+    #rewards_list keeps a sum of each rewards at a certain action until prior to a time
+    if time == 0:
+        return 0 #no action has been taken yet since time is 0
+    else:
+        sum_rewards_until_t = sum(rewards_list[t][action] for t in range(time))
+        num_times_action_recorded = action_record[:time].count(action)
+        if num_times_action_recorded == 0:
+            return 0 #avoids a division by 0
+        Qta = sum_rewards_until_t / num_times_action_recorded
+    return Qta 
