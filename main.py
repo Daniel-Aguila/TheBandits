f25a0e18f1c7b9bdb12796f6e86115504ebe07f3
Daniel Aguila <danielbaguila@gmail.com>
2024-01-03 19:14:15 -0600

    Incorporated Epsilon Greedy as a class

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-from argparse import Action
 import numpy as np
 import cv2
 import random
 
 #Function to select one of the actions with the highest estimated value
-def argmax(Qa):
-    max_Qa = max(Qa)
-    max_Qa_list = []
-    for i,value in enumerate(Qa):
-        if value == max_Qa:
-            max_Qa_list.append(i)
-    return random.choice(max_Qa_list) #randomly choose if multiple indixes have same value
+class EpsilonGreedy:
+    def __init__(self, Qa, epsilon):
+        self.Qa = Qa
+        self.epsilon = epsilon
 
-def epsilon_greedy(Qa,epsilon): #Incorporate epsilon to explore
-    random_number = np.random.random()
-    if random_number < epsilon:
-        return random.choice(range(len(Qa)))
-    else:
-        return argmax(Qa)
+    def _argmax(self):
+        max_Qa = max(self.Qa)
+        max_Qa_list = []
+        for i,value in enumerate(self.Qa):
+            if value == max_Qa:
+                max_Qa_list.append(i)
+        return random.choice(max_Qa_list) #randomly choose if multiple indixes have same value
+
+    def epsilon_greedy(self): #Incorporate epsilon to explore
+        random_number = np.random.random()
+        if random_number < self.epsilon:
+            return random.choice(range(len(self.Qa)))
+        else:
+            return self._argmax(self.Qa)
 
 #implement QtA which gives the Q value at a current action given a certain time
 def getQtA(action, time, rewards_list, action_record):
