fa7df070720a2a498e57563f861ab81f80957f7f
Daniel Aguila <danielbaguila@gmail.com>
2024-01-05 20:46:13 -0600

    Implemented the training loop for a stationary bandit problem

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 #Function to select one of the actions with the highest estimated value
 class EpsilonGreedy:
-    def __init__(self, Q_values, epsilon):
+    def __init__(self, Q_values, epsilon=0):
         self.Q_values = Q_values
         self.epsilon = epsilon
 
@@ -16,7 +16,7 @@ class EpsilonGreedy:
                 max_Q_values_list.append(i)
         return random.choice(max_Q_values_list) #randomly choose if multiple indixes have same value
 
-    def epsilon_greedy(self): #Incorporate epsilon to explore
+    def select_action(self): #Incorporate epsilon to explore
         random_number = np.random.random()
         if random_number < self.epsilon:
             return random.choice(range(len(self.Q_values)))
@@ -40,3 +40,28 @@ def getQtA(action, time, rewards_list, action_record):
 def updateQ(old_estimate, learning_rate, reward):
     new_estimate = old_estimate + learning_rate * (reward-old_estimate)
     return new_estimate
+
+if __name__ == "__main__":
+    k = 10
+    Q_values = np.zeros(k)
+    action_counts = np.zeros(k)
+
+    steps = 1000
+    alpha = 0.01
+    true_action_values = np.random.normal(0,1,k)
+
+    epsilon_greedy = EpsilonGreedy(Q_values,0.01)
+
+    for step in range(steps):
+        action = epsilon_greedy.select_action()
+
+        reward = np.random.normal(true_action_values[action], 1)
+        action_counts[action] += 1
+
+        Q_values[action] = updateQ(Q_values[action], alpha, reward)
+        
+    print("Estimated action values:", Q_values)
+    print("True action values:", true_action_values)
+    print("Optimal action:", np.argmax(true_action_values))
+    print("Most selected action:", np.argmax(action_counts))
+
